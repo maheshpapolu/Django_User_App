f00e3e47c65b3e90eb732043b0b1977fa61a0fe0
maheshpapolu <maheshnaidu9666@gmail.com>
2022-08-01 14:47:27 +0530

    [mahesh]added_UC2_User_App_Modified

--- user/views.py
+++ user/views.py
@@ -1,7 +1,10 @@
-import json
+from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.http import JsonResponse
 # from django.shortcuts import render
 from user.models import UserDetails
+import json
 
 
 # Create your views here.
@@ -26,31 +29,33 @@ def registration(request):
     """
     try:
         input_data = json.loads(request.body)
-        data = UserDetails.objects.create(username=input_data.get("username"), password=input_data.get("password"),
-                                          email=input_data.get("email"), phone_number=input_data.get("phone_number"),
-                                          location=input_data.get("location"))
+        data = UserDetails.objects.create_user(username=input_data.get("username"), password=input_data.get("password"),
+                                               email=input_data.get("email"),
+                                               phone_number=input_data.get("phone_number"),
+                                               location=input_data.get("location"))
         print(data)
         return JsonResponse({"message": "running fine"})
-    except Exception as e:
-        print(e)
-        return JsonResponse({"message": "error"})
+    except IntegrityError:
+        exception = {'Exception': 'Already Exits...!'}
+        return JsonResponse(exception)
 
 
 def user_login(request):
     """
     create the user_login for getting into app
     """
+    input_data = json.loads(request.body)
     try:
-        input_data = json.loads(request.body)
+
         if request.method == "POST":
             username = input_data.get('username')
             password = input_data.get('password')
-            login_user = UserDetails.objects.filter(username=username, password=password)
+            login_user = authenticate(username=username, password=password)
             if not login_user:
                 data = {'message': 'please check your username and password'}
                 return JsonResponse(data)
-            else:
-                return JsonResponse({'message': 'correct data entered'})
-    except Exception as e:
-        print(e)
-        return JsonResponse({'message': 'error in your program'})
+            return JsonResponse({'message': 'logged successfully'})
+        return JsonResponse({'message': 'please check your request method'})
+    except ValidationError:
+        exception = {'Exception': " 'value doesn't Exits...!'"}
+        return JsonResponse(exception)
